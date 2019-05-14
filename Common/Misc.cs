e754a5c067a0c7142c9a617e4968dbc33971feb1
dinar <none@none>
2019-05-14 16:55:48 +0300

    Misc.IsSameProcessExists()

---
+++ Common/Misc.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Common
+{
+	public static class Misc
+	{
+		/// <summary>
+		/// Возвращает True, если существует процесс запущенный из того же файла, что и текущий.
+		/// </summary>
+		public static bool IsSameProcessExists()
+		{
+			Process currentProcess = Process.GetCurrentProcess();
+			foreach (Process process in Process.GetProcessesByName(currentProcess.ProcessName))
+			{
+				if (process.Id != currentProcess.Id
+				    && process.MainModule != null
+				    && currentProcess.MainModule != null
+				    && process.MainModule.FileName == currentProcess.MainModule.FileName)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}

460a439a441df70151d43f4bfa43278aef645292
dinar <xewelus@gmail.com>
2020-01-14 15:12:39 +0300

    FS.Combine, AppFolder, GetAppFolder

--- Common/FS.cs
+++ Common/FS.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Common
@@ -64,6 +65,53 @@ namespace Common
 				return path1[0] != path2[0];
 			}
 			return false;
+		}
+
+		public static string Combine(params string[] paths)
+		{
+			if (paths.Length == 0) return string.Empty;
+
+			string result = paths[0];
+			for (int i = 1; i < paths.Length; i++)
+			{
+				result = Path.Combine(result, paths[i]);
+			}
+			return result;
+		}
+
+		private static string appFolder;
+		public static string AppFolder
+		{
+			get
+			{
+				if (appFolder == null)
+				{
+					appFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+				}
+				return appFolder;
+			}
+		}
+
+		public static string GetAppFolder(string path, bool trim = false)
+		{
+			if (!Path.IsPathRooted(path))
+			{
+				if (string.IsNullOrEmpty(path))
+				{
+					path = AppFolder;
+				}
+				else
+				{
+					if (trim)
+					{
+						path = path.Trim();
+					}
+
+					// если путь задан как отностильный, то помещаем файл в папку сервиса
+					path = Path.Combine(AppFolder, path);
+				}
+			}
+			return Path.GetFullPath(path);
 		}
 	}
 }

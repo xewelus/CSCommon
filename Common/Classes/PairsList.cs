4185ce901ef2d4af30c67c658db013b2a928176e
dinar <xewelus@gmail.com>
2020-01-14 13:17:46 +0300

    PairsList

---
+++ Common/Classes/PairsList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.Classes
+{
+	/// <summary>
+	/// Список пар "ключ"-"значение" упорядоченных по индексу.
+	/// </summary>
+	[Serializable]
+	public class PairsList<TKey, TValue> : List<KeyValuePair<TKey, TValue>>
+	{
+		public void Add(TKey key, TValue value)
+		{
+			this.Add(new KeyValuePair<TKey, TValue>(key, value));
+		}
+	}
+}

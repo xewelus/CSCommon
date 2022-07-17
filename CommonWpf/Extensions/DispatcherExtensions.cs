472cde451cb2bdc7df7a201db41ff32f7b762982
xewelus <xewelus@gmail.com>
2022-07-17 15:55:05 +0300

    Dispatcher.InvokeAction

---
+++ CommonWpf/Extensions/DispatcherExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace CommonWpf.Extensions
+{
+	public static class DispatcherExtensions
+	{
+		public static void InvokeAction(this Dispatcher dispatcher, Action action)
+		{
+			if (dispatcher.CheckAccess())
+			{
+				action();
+			}
+			else
+			{
+				dispatcher.Invoke(action);
+			}
+		}
+	}
+}

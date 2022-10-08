803cbffebc7327acfc08a814e9a6e3c7907709f2
dinar <dinar.mobile@gmail.com>
2022-10-08 04:55:58 +0300

    Diag.SaveAndOperLog()

---
+++ Common/Classes/Diagnostic/Diag.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Common.Classes.Diagnostic
+{
+	/// <summary>
+	/// Contains helpful methods for problems diagnostics.
+	/// </summary>
+	public static class Diag
+	{
+		/// <summary>
+		/// Save text in file (usually as error) and open this file in default application.
+		/// </summary>
+		public static string SaveAndOpenLog(string text, string prefix = "error_")
+		{
+			string path = FS.GetAppPath($"{prefix}{DateTime.Now:yyMMdd_HHmmss_fff}.log");
+
+			try
+			{
+				File.AppendAllText(path, text);
+				FS.OpenInDefaultApp(path);
+				return path;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception($"Error while processing log '{path}'.", ex);
+			}
+		}
+	}
+}

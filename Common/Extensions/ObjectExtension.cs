d69e88da5504aca520b6f8636d6b58b006835160
dinar <dinar.mobile@gmail.com>
2023-01-23 18:41:22 +0300

    .net7

--- Common/Extensions/ObjectExtension.cs
+++ Common/Extensions/ObjectExtension.cs
@@ -49,17 +49,5 @@ namespace Common
 			}
 			return true;
 		}
-
-		public static T DeepCopy<T>(this T other)
-		{
-			using (MemoryStream ms = new MemoryStream())
-			{
-				BinaryFormatter formatter = new BinaryFormatter();
-				formatter.Context = new StreamingContext(StreamingContextStates.Clone);
-				formatter.Serialize(ms, other);
-				ms.Position = 0;
-				return (T)formatter.Deserialize(ms);
-			}
-		}
 	}
 }
